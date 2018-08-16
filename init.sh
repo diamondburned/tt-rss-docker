be1eee61edd79046eff6bb9a721f2cf29338cf85
diamondburned <datutbrus@gmail.com>
2018-08-16 12:46:39 -0700

    workaround for SELF_URL_PATH_CHECKS

--- init.sh
+++ init.sh
@@ -3,12 +3,12 @@
 # Initiate and pull from master
 #rm -rf /var/www/tt-rss
 git clone --progress --verbose --depth=1 https://git.tt-rss.org/fox/tt-rss.git /var/www/tt-rss
-
+echo "define('_SKIP_SELF_URL_PATH_CHECKS', true);" | tee /var/www/tt-rss/config.php
 #echo "${NGINX_conf:1:-1}" | tee /etc/nginx/nginx.conf &> /dev/null 
 #echo "${NGINX_fastcgi_conf:1:-1}" | tee /etc/nginx/fastcgi.conf &> /dev/null
 
 ln -s /usr/share/webapps/phpmyadmin /var/www/tt-rss/
-ln -s /etc/phpmyadmin/config.inc.php /var/www/tt-rss/phpmyadmin-conf.php
+ln -s /etc/phpmyadmin/config.inc.php /var/www/tt-r	ss/phpmyadmin-conf.php
 
 # Initialize NGINX
 mkdir -p /run/nginx
